f52c5e7f20c16585fc8ba012e0d5288021b083b5
Mm2PL <miau@mail.kotmisia.pl>
2023-02-01 22:41:14 +0100

    Move chatterino global namespace shit into a namespace

--- index.lua
+++ index.lua
@@ -3,33 +3,33 @@ print("FeelsDankMan!!!!")
 
 function test(chn)
     -- chn is a string, the channel name
-    system_msg(chn, "test")
+    c2.system_msg(chn, "test")
 end
 
 function cbTesting(ctx)
     print(ctx)
-    system_msg(ctx.channelName, "Hello world from lua!")
-    system_msg(ctx.channelName, "This is #" .. ctx.channelName)
-    system_msg(ctx.channelName, "The words given were " .. table.concat(ctx.words, " "))
-    send_msg(ctx.channelName, "/openurl http://localhost")
+    c2.system_msg(ctx.channelName, "Hello world from lua!")
+    c2.system_msg(ctx.channelName, "This is #" .. ctx.channelName)
+    c2.system_msg(ctx.channelName, "The words given were " .. table.concat(ctx.words, " "))
+    c2.send_msg(ctx.channelName, "/openurl http://localhost")
 end
 
 function cmdEval(ctx)
     table.remove(ctx.words, 1)
     local input = table.concat(ctx.words, " ")
     local source = "return " .. input
-    system_msg(ctx.channelName, ">>>" .. input)
-    system_msg(ctx.channelName, "<< " .. tostring(load(source)()))
+    c2.system_msg(ctx.channelName, ">>>" .. input)
+    c2.system_msg(ctx.channelName, "<< " .. tostring(load(source)()))
 end
-register_command("/eval", cmdEval)
+c2.register_command("/eval", cmdEval)
 
-register_command("testing", cbTesting)
+c2.register_command("testing", cbTesting)
 
 
 function totally_not_an_exploit_xd(ctx)
     for line in io.lines("/proc/self/stat") do
-        system_msg(ctx.channelName, line)
+        c2.system_msg(ctx.channelName, line)
     end
 end
-register_command("/read_files", totally_not_an_exploit_xd)
+c2.register_command("/read_files", totally_not_an_exploit_xd)
 print("RELOADED")
