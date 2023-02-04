b1a5236cae63f201e66a05a91faded70b0342b66
Mm2PL <miau@mail.kotmisia.pl>
2023-02-04 01:29:01 +0100

    Clean up index.lua and make /eval a little better

--- index.lua
+++ index.lua
@@ -1,35 +1,16 @@
-
 print("FeelsDankMan!!!!")
-
-function test(chn)
-    -- chn is a string, the channel name
-    c2.system_msg(chn, "test")
-end
-
-function cbTesting(ctx)
-    print(ctx)
-    c2.system_msg(ctx.channelName, "Hello world from lua!")
-    c2.system_msg(ctx.channelName, "This is #" .. ctx.channelName)
-    c2.system_msg(ctx.channelName, "The words given were " .. table.concat(ctx.words, " "))
-    c2.send_msg(ctx.channelName, "/openurl http://localhost")
-end
 
 function cmdEval(ctx)
     table.remove(ctx.words, 1)
     local input = table.concat(ctx.words, " ")
     local source = "return " .. input
     c2.system_msg(ctx.channelName, ">>>" .. input)
-    c2.system_msg(ctx.channelName, "<< " .. tostring(load(source)()))
-end
-c2.register_command("/eval", cmdEval)
-
-c2.register_command("testing", cbTesting)
-
-
-function totally_not_an_exploit_xd(ctx)
-    for line in io.lines("/proc/self/stat") do
-        c2.system_msg(ctx.channelName, line)
+    local f, err = load(source)
+    if f == nil then
+        c2.system_msg(ctx.channelName "!<" .. tostring(err))
+    else
+        c2.system_msg(ctx.channelName, "<< " .. tostring(f()))
     end
 end
-c2.register_command("/read_files", totally_not_an_exploit_xd)
-print("RELOADED")
+
+c2.register_command("/eval", cmdEval)
