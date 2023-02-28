e2e2980c8037fd17c4810c80d8d91f42bd47be8c
Mm2PL <miau@mail.kotmisia.pl>
2023-02-28 22:32:54 +0100

    channelName -> channel_name

--- init.lua
+++ init.lua
@@ -4,12 +4,12 @@ function cmdEval(ctx)
     table.remove(ctx.words, 1)
     local input = table.concat(ctx.words, " ")
     local source = "return " .. input
-    c2.system_msg(ctx.channelName, ">>>" .. input)
+    c2.system_msg(ctx.channel_name, ">>>" .. input)
     local f, err = load(source)
     if f == nil then
-        c2.system_msg(ctx.channelName "!<" .. tostring(err))
+        c2.system_msg(ctx.channel_name "!<" .. tostring(err))
     else
-        c2.system_msg(ctx.channelName, "<< " .. tostring(f()))
+        c2.system_msg(ctx.channel_name, "<< " .. tostring(f()))
     end
 end
 
