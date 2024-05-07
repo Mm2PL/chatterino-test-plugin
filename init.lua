28cc6cf44ddacd3ac1905ebdf3c06ca1ad9125a1
Mm2PL <miau@mail.kotmisia.pl>
2024-05-07 14:59:16 +0200

    Update test plugin

--- init.lua
+++ init.lua
@@ -60,7 +60,40 @@ function cmd_filesystem(ctx)
     end
 end
 
-function onCompletion(query, full_text_content, cursor_position, is_first_word)
+function cmd_http(ctx)
+    table.remove(ctx.words, 1)
+    local url = "http://localhost:8080/" .. table.concat(ctx.words, "%20")
+    local request = c2.HTTPRequest.create("Post", url)
+    print('Created request!')
+    _G.request = request
+    request:set_timeout(1000)
+    request:set_payload("TEST!")
+    request:set_header("X-Test", "Testing!")
+    request:set_header("X-Test", "Testing 2!")
+    request:set_header("Content-Type", "text/plain")
+    request:on_success(function (res)
+        print('Success!')
+        print(res.data)
+        ctx.channel:add_system_message("Success! " .. res.status)
+    end)
+    request:on_error(function (res)
+        print('Error!')
+        print(res.data)
+        print('--------')
+        ctx.channel:add_system_message("Error! " .. (res.status or "nil") .. " " .. res.error)
+    end)
+    request:finally(function ()
+        print('Finally')
+        ctx.channel:add_system_message("Finally!")
+    end)
+    request:execute()
+end
+
+function onCompletion(ev)
+    local cursor_position = ev.cursor_position
+    local full_text_content = ev.full_text_content
+    local is_first_word = ev.is_first_word
+    local query = ev.query
     local list = {
         hide_others = false,
         values = {},
@@ -77,4 +110,5 @@ end
 c2.register_command("/eval", cmdEval)
 c2.register_command("/foo", cmdFoo)
 c2.register_command("/test-fs", cmd_filesystem)
+c2.register_command("/test-http", cmd_http)
 c2.register_callback(c2.EventType.CompletionRequested, onCompletion)
